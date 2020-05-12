1d6f3b1d3b41e2c37acc69ac36c7fe7cbfa5687f
Luiz Ferreira <luiz.ferreira@outlook.com>
2020-05-12 09:55:16 -0300

    Updates

---
+++ precache-manifest.10d4dbc38ff916f0328c6cd9e42eda2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65ff4cd34c5af15833cc2004276459f8",
+    "url": "/github-explorer/index.html"
+  },
+  {
+    "revision": "b13718adf5e73d486919",
+    "url": "/github-explorer/static/js/2.de773d42.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-explorer/static/js/2.de773d42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "918ebee4bdab3266e168",
+    "url": "/github-explorer/static/js/main.245ee906.chunk.js"
+  },
+  {
+    "revision": "6bdb3542f7c98cf05f87",
+    "url": "/github-explorer/static/js/runtime-main.307ea7e1.js"
+  },
+  {
+    "revision": "0b819d63338019a0f1cc7263e9c166ae",
+    "url": "/github-explorer/static/media/github-background.0b819d63.svg"
+  },
+  {
+    "revision": "04f6798f67fd4e48d053d05d18d9bd83",
+    "url": "/github-explorer/static/media/logo.04f6798f.svg"
+  }
+]);
